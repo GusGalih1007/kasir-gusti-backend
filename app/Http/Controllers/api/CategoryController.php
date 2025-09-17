1851bed4831fb6dcfe86762123e07567bc2d42f0
GusGalih1007 <gustimuhammadgalih@gmail.com>
2025-09-17 16:28:19 +0700

    first push

---
+++ app/Http/Controllers/api/CategoryController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Http\Resources\CategoryResource;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $category = Category::latest()->paginate(5);
+
+        return new CategoryResource(200, 'Success', $category);
+    }
+
+    public function store (Request $request)
+    {
+        $validate = Validator::make($request->all(), [
+            'name' => 'required',
+            'description' => 'required',
+            'parent_id' => 'nullable|numeric',
+        ]);
+
+        if ($validate->fails())
+        {
+            return response()->json($validate->errors(), 422);
+        }
+
+        $category = Category::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'parent_id' => $request->parent_id
+        ]);
+
+        return new CategoryResource(201, 'Success', $category);
+    }
+    public function show($id)
+    {
+        $category = Category::findOrFail($id);
+
+        return new CategoryResource(200, 'Success', $category);
+    }
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'description' => 'required',
+            'parent_id' => 'nullable'
+        ]);
+
+        if($validator->fails())
+        {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $category = Category::findOrFail($id);
+
+        $category->update([
+            'name' => $request->name,
+            'description' => $request->description,
+            'parent_id' => $request->parent_id
+        ]);
+
+        return new CategoryResource(201, 'Success', $category);
+    }
+    public function destroy($id)
+    {
+        $category = Category::findOrFail($id);
+        $category->delete();
+
+        return new CategoryResource(204, 'Deleted Success', null);
+    }
+}
