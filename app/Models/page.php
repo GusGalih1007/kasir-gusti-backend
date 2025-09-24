1eb9adf200fa997f46821dc79020dac030a73c60
GusGalih1007 <gustimuhammadgalih@gmail.com>
2025-09-24 16:22:11 +0700

    start page endpoint

---
+++ app/Models/page.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class page extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'pages';
+    protected $primaryKey = 'page_code';
+    protected $fillable = ['page_name', 'action'];
+}
---
+++ database/migrations/2025_09_24_071603_create_pages_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->id('page_code');
+            $table->string('page_name', 50);
+            $table->string('action', 10);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pages');
+    }
+};
